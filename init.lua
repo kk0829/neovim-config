7b9b9a9636cf3a2b21e1db42529966537aaf89ca
wanggan.kk <wanggan.kk@bytedance.com>
2022-08-01 22:35:39 +0800

    feat: 初始化快捷键配置

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@ require('basic')
 -- Packer 插件管理
 require("plugins")
 
+-- 快捷键映射
+require("keybindings")
+
 -- 主题设置 （新增）
 require("colorscheme")
---
+++ lua/keybindings.lua
@@ -0,0 +1,45 @@
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- leader key 为空
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+-- 本地变量
+local map = vim.api.nvim_set_keymap
+
+-- $跳到行尾不带空格 (交换$ 和 g_)
+map("v", "$", "g_", opt)
+map("v", "g_", "$", opt)
+map("n", "$", "g_", opt)
+map("n", "g_", "$", opt)
+
+-- 命令行下 Ctrl+j/k  上一个下一个
+map("c", "<C-j>", "<C-n>", { noremap = false })
+map("c", "<C-k>", "<C-p>", { noremap = false })
+
+map("n", "<leader>w", ":w<CR>", opt)
+map("n", "<leader>wq", ":wqa!<CR>", opt)
+
+-- fix :set wrap
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- 上下滚动浏览
+map("n", "<C-j>", "5j", opt)
+map("n", "<C-k>", "5k", opt)
+map("v", "<C-j>", "5j", opt)
+map("v", "<C-k>", "5k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-u>", "10k", opt)
+map("n", "<C-d>", "10j", opt)
